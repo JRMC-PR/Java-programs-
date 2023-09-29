4c27e39cb24914bbf5e55af8f08f3e6c385dd3d6
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-29 13:53:42 -0400

    rearanging

---
+++ Hlb-2tri-In-Java/last_digit.java
@@ -0,0 +1,10 @@
+package Hlb-2 tri-In-Java;
+
+public class last_digit {
+    public static void main(String[] args) {
+        int number = (int) (Math.random() * 20001 -10000); // & generate a random number
+        //& chekc value
+        if number < 0
+        System.out.println("Last digit of " + number)
+    } // end main
+} // end class positive_or_negative
---
+++ Hlb-2tri-In-Java/positive_or_negative.java
@@ -0,0 +1,27 @@
+
+/*
+ &This program will assign a random signed number to the variable number each time it is executed.
+&The variable number will store a different value every time you will run this program
+&The output of the program should be:
+&The number, followed by
+&if the number is greater than 0: is positive
+&if the number is 0: is zero
+&if the number is less than 0: is negative
+&followed by a new line
+ */
+import java.util.*;
+
+public class positive_or_negative {
+
+    public static void main(String[] args) {
+        //& generate a random number
+        int number = (int) (Math.random() * 100 + 1); // & generate a random number
+        // verify and print
+        if (number > 0)
+            System.out.println(number + " is positive");
+        else if (number == 0)
+            System.out.println(number + " is zero");
+        else
+            System.out.println(number + " is negative");
+    } // end main
+} // end class positive_or_negative
