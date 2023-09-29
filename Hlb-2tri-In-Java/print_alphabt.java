e9034cb37fc7ce7011c616cc2718c55e91bb796b
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-29 14:37:12 -0400

    exercise 3

--- Hlb-2tri-In-Java/print_alphabt.java
+++ Hlb-2tri-In-Java/print_alphabt.java
@@ -1,4 +1,18 @@
-
+/*
+&Write a program that prints the ASCII alphabet,
+&in lowercase, not followed by a new line.
+&Print all the letters except q and e
+&You can only use one print function with string format
+&You can only use one loop in your code
+&You are not allowed to store characters in a variable
+&You are not allowed to import any module
+*/
 public class print_alphabt {
-
-}
+    public static void main(String[] args) {
+        char c;
+        for (c = 'a'; c <= 'z'; c++) {
+            if (c != 'q' && c != 'e')
+                System.out.print(c);
+        } // end for
+    } // end main
+} // end class print_alphabt
