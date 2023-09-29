99f4ca6358cfa1be70b90fabf7e42348c3404776
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-29 14:27:37 -0400

    adding instruction

--- Hlb-2tri-In-Java/print_alphabet.java
+++ Hlb-2tri-In-Java/print_alphabet.java
@@ -1,4 +1,11 @@
-
+/*
+&Write a program that prints the ASCII alphabet,
+&in lowercase, not followed by a new line.
+&Use only one print function with string format
+&Use only one loop in your code
+&You are not allowed to store characters in a variable
+&You are not allowed to import any module
+*/
 public class print_alphabet {
     public static void main(String[] args) {
         char c;
