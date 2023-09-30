9c14e8e284eb96ae16a4395c1de85b8c64709703
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-29 21:24:12 -0400

    adding instructions

--- Hlb-2tri-In-Java/add.java
+++ Hlb-2tri-In-Java/add.java
@@ -1,3 +1,10 @@
+/*
+ Create a new Java class called Add.
+Define a public static method called add that takes two int parameters (a and b) and returns an int.
+Inside the add method, add a and b together using the + operator.
+Return the result of the addition.
+In the main method, call the add method with test values for a and b to verify that it works correctly.
+*/
 public class add {
     /*
      * Define a public static method called add that takes two int parameters and
