a4dca668d7cf019d117e7347bba0a1ded4ba9dac
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-09-29 21:15:38 -0400

    exercise 10

---
+++ Hlb-2tri-In-Java/add.java
@@ -0,0 +1,19 @@
+public class add {
+    /*
+     * Define a public static method called add that takes two int parameters and
+     * returns an int.
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    } // end add
+
+    /*
+     * main method
+     */
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+        System.out.println(add(98, 0));
+        System.out.println(add(100, -2));
+    } // end main
+
+}
