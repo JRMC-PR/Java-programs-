1abe9aa26127c6fc572caedaaa72b781fa3a3800
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-01 15:02:08 -0400

    exercise 1

--- Hlb_2tri_In_Java/import_modules/calculation.java
+++ Hlb_2tri_In_Java/import_modules/calculation.java
@@ -1,3 +1,36 @@
+
+/**
+ * Apply intrsuctions to java
+ * Write a program that imports functions from the file calculator_1.py,
+ * does some Maths, and prints the result.
+ * Do not use the function print (with string format to display integers) more
+ * than 4 times
+ * You have to define:
+ * the value 10 to a variable a
+ * the value 5 to a variable b
+ * and use those two variables only, as arguments when calling functions
+ * (including print)
+ * a and b must be defined in 2 different lines: a = 10 and another b = 5
+ * Your program should call each of the imported functions. See example below
+ * for format
+ * the word calculator_1 should be used only once in your file
+ * You are not allowed to use * for importing or __import__
+ * Your code should not be executed when imported
+ */
+import imports.calculator_1;
+
 public class calculation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        // Add a and b
+        System.out.println(a + " + " + b + " = " + calculator_1.add(a, b));
+        // subtract a and b
+        System.out.println(a + " - " + b + " = " + calculator_1.sub(a, b));
+        // multiply a and b
+        System.out.println(a + " * " + b + " = " + calculator_1.mul(a, b));
+        // divide a and b
+        System.out.println(a + " / " + b + " = " + calculator_1.div(a, b));
+    } // end main
 
 }
---
+++ Hlb_2tri_In_Java/import_modules/imports/calculator_1.java
@@ -0,0 +1,52 @@
+package imports;
+
+/**
+ * class includes imports for the file names:
+ * *calculation.java
+ */
+
+public class calculator_1 {
+    /**
+     * My addition function
+     *
+     * @param a first integer
+     * @param b second integer
+     * @return the return value. a + b
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * My subtraction function
+     *
+     * @param a first integer
+     * @param b second integer
+     * @return the return value. a - b
+     */
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    /**
+     * My multiplication function
+     *
+     * @param a first integer
+     * @param b second integer
+     * @return the return value. a * b
+     */
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * My division function
+     *
+     * @param a first integer
+     * @param b second integer
+     * @return the return value. a / b
+     */
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}
