8400d2b791dda884f56bdea86437673f9a7eb27e
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-10-01 14:23:31 -0400

    update

---
+++ Hlb_2tri_In_Java/import_modules/add.java
@@ -0,0 +1,11 @@
+import imports.add_0;
+
+public class add {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int result = add_0.Add_0(a, b);
+        System.out.printf("%d + %d = %d\n", a, b, result);
+    }
+}
---
+++ Hlb_2tri_In_Java/import_modules/imports/add_0.java
@@ -0,0 +1,8 @@
+package imports;
+
+public class add_0 {
+
+    public static int Add_0(int a, int b) {
+        return a + b;
+    } // end Add method
+}
